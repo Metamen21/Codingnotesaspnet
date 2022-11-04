bcf044a4b433a6e5afe3f9fbe91cc416779f3b7e
Ruthik224 <60350995+Ruthik224@users.noreply.github.com>
2022-11-05 00:03:07 +0530

    Update Notes1.cs

--- Notes1.cs
+++ Notes1.cs
@@ -9,4 +9,26 @@
                 return;
             }
             
+
+
+SELECT * FROM [dbo].[BloodGroup]
+ORDER BY [BLOODGROUPID]
+OFFSET 5 ROWS
+FETCH  NEXT 3 ROWS ONLY;
+
+DECLARE @PageNumber AS INT
+DECLARE @RowsOfPage AS INT
+DECLARE @SortingCol AS VARCHAR(100) ='FruitName'
+DECLARE @SortType AS VARCHAR(100) = 'DESC'
+SET @PageNumber=1
+SET @RowsOfPage=4
+SELECT FruitName,Price FROM SampleFruits
+ORDER BY 
+CASE WHEN @SortingCol = 'Price' AND @SortType ='ASC' THEN Price END ,
+CASE WHEN @SortingCol = 'Price' AND @SortType ='DESC' THEN Price END DESC,
+CASE WHEN @SortingCol = 'FruitName' AND @SortType ='ASC' THEN FruitName END ,
+CASE WHEN @SortingCol = 'FruitName' AND @SortType ='DESC' THEN FruitName END DESC
+OFFSET (@PageNumber-1)*@RowsOfPage ROWS
+FETCH NEXT @RowsOfPage ROWS ONLY
+
             
